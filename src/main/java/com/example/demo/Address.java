2840836720a1c2a3aba2b14d113ddc241da87fc2
lakshanamal <lakshanamal100@gmail.com>
2021-07-23 09:50:33 +0530

    learned about lombark

---
+++ src/main/java/com/example/demo/Address.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+public class Address {
+    private String country;
+    private String postCOde;
+    private String city;
+}
---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Data
+@Document
+public class Student {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Gender gender;
+    private Address address;
+    private List<String> favoriteSubject;
+    private BigDecimal totalSpendInBook;
+    private ZonedDateTime created;
+}
