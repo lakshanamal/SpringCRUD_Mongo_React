86813b5128c25ff9f8476a34a23ee053debc9545
lakshanamal <lakshanamal100@gmail.com>
2021-07-27 00:19:07 +0530

    new  project added

--- src/test/java/com/lakshan/crud/CrudApplicationTests.java
+++ src/test/java/com/lakshan/crud/CrudApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.lakshan.crud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class CrudApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
