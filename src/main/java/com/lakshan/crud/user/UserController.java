88e4ae0c064ed0718e725b91c6260a2f75e2499d
lakshanamal <lakshanamal100@gmail.com>
2021-07-27 09:06:53 +0530

    get mapping added

---
+++ src/main/java/com/lakshan/crud/user/UserController.java
@@ -0,0 +1,23 @@
+package com.lakshan.crud.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAlluser(){
+        return ResponseEntity.ok(userService.getUsers());
+    }
+}
---
+++ src/main/java/com/lakshan/crud/user/UserService.java
@@ -0,0 +1,18 @@
+package com.lakshan.crud.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+}
